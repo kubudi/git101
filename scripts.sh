823a10a57d7ec96343de2c44edaec6919fadef40
Mehmet Ali Gozaydin <kubudik@gmail.com>
2015-11-28 11:35:37 +0200

    implement prev

--- scripts.sh
+++ scripts.sh
@@ -11,3 +11,17 @@ function next() {
   newbr=`git br | grep $next | xargs`
   git co $newbr
 }
+
+function prev() {
+  number=`git br | grep \* | awk -F' ' '{print $2}' | awk -F'-' '{print $1}'`
+  nextnum=$(($number - 1))
+  next=""
+  if [ $nextnum -lt 10 ]
+  then
+   next="0$nextnum"
+  else
+   next=$nextnum
+  fi
+  newbr=`git br | grep $next | xargs`
+  git co $newbr
+}
