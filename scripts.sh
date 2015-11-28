de7d101b85572d72a203251549bb21742835d927
Mehmet Ali Gozaydin <kubudik@gmail.com>
2015-11-28 11:32:29 +0200

    implement next

---
+++ scripts.sh
@@ -0,0 +1,13 @@
+function next() {
+  number=`git br | grep \* | awk -F' ' '{print $2}' | awk -F'-' '{print $1}'`
+  nextnum=$(($number + 1))
+  next=""
+  if [ $nextnum -lt 10 ]
+  then
+   next="0$nextnum"
+  else
+   next=$nextnum
+  fi
+  newbr=`git br | grep $next | xargs`
+  git co $newbr
+}
